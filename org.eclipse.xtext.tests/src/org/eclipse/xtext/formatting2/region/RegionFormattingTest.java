3dc951cd0deadd54cac6d4fd3530e38070a9c804
Bernd Lehmann <bernd.lehmann@posteo.de>
2024-11-01 11:23:12 +0100

    Enhance and document tests to check formatting of selected regions
    Add a test to check that the formatter formats relative to the parent of
    the selected region.
    
    Signed-off-by: Bernd Lehmann <bernd.lehmann@posteo.de>

--- org.eclipse.xtext.tests/src/org/eclipse/xtext/formatting2/region/RegionFormattingTest.java
+++ org.eclipse.xtext.tests/src/org/eclipse/xtext/formatting2/region/RegionFormattingTest.java
@@ -31,9 +31,16 @@ public class RegionFormattingTest {
 	@Inject
 	private FormatterTestHelper formatterTestHelper;
 
+	/**
+	 * In this test the parent of the selection (element B) is on the same
+	 * indentation level (level 1) as it would be after formatting the entire
+	 * document (hitting Ctrl+Shift+F without selection). Expectation is that
+	 * the selected line (ref A) is formatted to level 2 which is also the same
+	 * as it would be after formatting without selection.
+	 */
 	@Ignore("until formatting issues are solved")
 	@Test
-	public void testSingleSelectedLineIsCorrectlyIndented() {
+	public void singleSelectedLineIsFormattedToIndentationLevel2_withParentOnLevel1() {
 		// @formatter:off
 		String input =
 				"package A\n" +
@@ -64,4 +71,43 @@ public class RegionFormattingTest {
 		});
 	}
 
+	/**
+	 * In this test the parent of the selection (element B) is NOT on the same
+	 * indentation level as it would be after formatting the entire document
+	 * (hitting Ctrl+Shift+F without selection). Expectation is that the
+	 * selected line (ref A) is formatted <b>relative</b> to the current parent
+	 * indentation level (level 0) and is indented to level 1.
+	 */
+	@Ignore("until formatting issues are solved")
+	@Test
+	public void singleSelectedLineIsFormattedToIndentationLevel1_withParentOnLevel0() {
+		// @formatter:off
+		String input =
+				"package A\n" +
+				"\n" +
+				"element A {\n" +
+				"element B {\n" +
+				"ref A\n" +
+				"}\n" +
+				"}\n";
+		String expected =
+				"package A\n" +
+				"\n" +
+				"element A {\n" +
+				"element B {\n" +
+				"\tref A\n" +
+				"}\n" +
+				"}\n";
+		// @formatter:on
+		String selection = "ref A\n";
+		ITextRegion selectedRegion = new TextRegion(input.indexOf(selection), selection.length());
+
+		formatterTestHelper.assertFormatted(formatterTestRequest -> {
+			formatterTestRequest.setToBeFormatted(input);
+			formatterTestRequest.setExpectation(expected);
+			FormatterRequest formatterRequest = new FormatterRequest();
+			formatterRequest.addRegion(selectedRegion);
+			formatterTestRequest.setRequest(formatterRequest);
+		});
+	}
 }
