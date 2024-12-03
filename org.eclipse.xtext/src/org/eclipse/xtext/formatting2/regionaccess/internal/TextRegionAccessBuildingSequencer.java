d1d3ec774ed879d5f7232fd0e90dc6dddc80fb71
Bernd Lehmann <bernd.lehmann@posteo.de>
2024-12-03 19:45:46 +0100

    Remove unused imports
    Signed-off-by: Bernd Lehmann <bernd.lehmann@posteo.de>

--- org.eclipse.xtext/src/org/eclipse/xtext/formatting2/regionaccess/internal/TextRegionAccessBuildingSequencer.java
+++ org.eclipse.xtext/src/org/eclipse/xtext/formatting2/regionaccess/internal/TextRegionAccessBuildingSequencer.java
@@ -17,8 +17,6 @@ import org.eclipse.xtext.AbstractRule;
 import org.eclipse.xtext.Action;
 import org.eclipse.xtext.Keyword;
 import org.eclipse.xtext.RuleCall;
-import org.eclipse.xtext.formatting2.regionaccess.IEObjectRegion;
-import org.eclipse.xtext.formatting2.regionaccess.IHiddenRegion;
 import org.eclipse.xtext.nodemodel.ICompositeNode;
 import org.eclipse.xtext.nodemodel.ILeafNode;
 import org.eclipse.xtext.resource.XtextResource;
